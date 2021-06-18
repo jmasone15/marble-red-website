6d8432c1a67da95f2f03a2e2c6236451d2d87aae
Jordan Masone <jordanmasone1@gmail.com>
2021-06-18 14:18:12 -0400

    Added album carousel.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import TopBar from "./components/TopBar";
+import Music from "./components/Music";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -14,7 +15,28 @@ function App() {
       <Container fluid className="App">
         <Row style={{ marginTop: "25%", marginBottom: "25%" }}>
           <Col style={{ textAlign: "center" }}>
-            <Header />
+            <Header id="home" />
+          </Col>
+        </Row>
+      </Container>
+      <Container fluid className="divider">
+        <Row>
+          <Col>
+            <h1 style={{ opacity: "0" }}>Blank Title</h1>
+          </Col>
+        </Row>
+      </Container>
+      <Container fluid className="Music">
+        <Row>
+          <Col>
+            <Music id="music" />
+          </Col>
+        </Row>
+      </Container>
+      <Container fluid className="divider">
+        <Row>
+          <Col>
+            <h1 style={{ opacity: "0" }}>Blank Title</h1>
           </Col>
         </Row>
       </Container>
---
+++ src/components/Music.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Carousel from "react-bootstrap/Carousel";
+
+export default function Music() {
+    return (
+        <Container>
+            <Row style={{ marginTop: "100px", marginBottom: "100px" }}>
+                <Col>
+                    <Carousel>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://via.placeholder.com/800x400.png?text=Album+Number+1"
+                                alt="Album #1"
+                            />
+                            <Carousel.Caption>
+                                <h3>First Album Title</h3>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://via.placeholder.com/800x400.png?text=Album+Number+2"
+                                alt="Album #2"
+                            />
+
+                            <Carousel.Caption>
+                                <h3>Second Album Title</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://via.placeholder.com/800x400.png?text=Album+Number+3"
+                                alt="Album #3"
+                            />
+
+                            <Carousel.Caption>
+                                <h3>Third Album Titlel</h3>
+                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
