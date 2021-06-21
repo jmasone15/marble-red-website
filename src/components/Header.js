eea37eb36c6c1420088dce7cf2d9eb2d011c7d23
Jordan Masone <jordanmasone1@gmail.com>
2021-06-21 19:18:37 -0400

    Fixed color scheme with client.:

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,13 @@ export default function Header() {
     return (
         <div className="this">
             <ul className="c-rainbow">
-                <li className="c-rainbow__layer c-rainbow__layer--yellow">MARBLE RED</li>
-                <li className="c-rainbow__layer c-rainbow__layer--green">MARBLE RED</li>
-                <li className="c-rainbow__layer c-rainbow__layer--blue">MARBLE RED</li>
                 <li className="c-rainbow__layer c-rainbow__layer--violet">MARBLE RED</li>
+                <li className="c-rainbow__layer c-rainbow__layer--blue">MARBLE RED</li>
+                <li className="c-rainbow__layer c-rainbow__layer--green">MARBLE RED</li>
                 <li className="c-rainbow__layer c-rainbow__layer--red">MARBLE RED</li>
+                <li className="c-rainbow__layer c-rainbow__layer--pink">MARBLE RED</li>
                 <li className="c-rainbow__layer c-rainbow__layer--orange">MARBLE RED</li>
-                <li className="c-rainbow__layer c-rainbow__layer--white">MARBLE RED</li>
+                <li className="c-rainbow__layer c-rainbow__layer--yellow">MARBLE RED</li>
             </ul>
         </div>
     )
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,19 +1,10 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 export default function TopBar() {
-
-    const history = useHistory();
-
-    const changePage = (e, target) => {
-        e.preventDefault();
-        history.push(target)
-    }
-
     return (
         <Navbar expand="xl" fixed="top" >
             <Navbar.Brand className="op0">Brand Test</Navbar.Brand>
@@ -23,7 +14,7 @@ export default function TopBar() {
                     <Row>
                         <Col>
                             <div>
-                                <h1 className="first-drop-text" style={{ display: "inline" }}><a href="#home" style={{ color: "inherit", textDecoration: "none" }}>Home</a></h1>
+                                <h1 className="first-drop-text" style={{ display: "inline" }}><a href="#" style={{ color: "inherit", textDecoration: "none" }}>Home</a></h1>
                             </div>
                         </Col>
                     </Row>
