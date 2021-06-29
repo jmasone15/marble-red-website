212f9c35685729ab24408e0568f3af57bfa50bdf
Jordan Masone <jordanmasone1@gmail.com>
2021-06-29 16:00:19 -0400

    Styled and animated contact modal.

--- src/components/ContactModal.js
+++ src/components/ContactModal.js
@@ -14,34 +14,58 @@ export default function ContactModal({ show, setShow }) {
         <Container>
             <Row>
                 <Col>
-                    <Modal show={show} onHide={handleClose} size="lg" aria-labelledby="example-modal-sizes-title-lg">
-                        <Modal.Header closeButton>
-                            <Modal.Title>Contact Form</Modal.Title>
-                        </Modal.Header>
-                        <Form>
-                            <Modal.Body>
-                                <Form.Group controlId="formBasicEmail">
-                                    <Form.Label>Email address</Form.Label>
-                                    <Form.Control type="email" placeholder="Enter email" />
-                                </Form.Group>
-
-                                <Form.Group controlId="formBasicPassword">
-                                    <Form.Label>Message</Form.Label>
-                                    <Form.Control as="textarea" placeholder="Send me a message :)" />
-                                </Form.Group>
-                            </Modal.Body>
-                            <Modal.Footer>
-                                <Button variant="secondary" onClick={handleClose}>
-                                    Close
-                                </Button>
-                                <Button variant="primary" onClick={handleClose}>
-                                    Submit
-                                </Button>
-                            </Modal.Footer>
-                        </Form>
+                    <Modal show={show} onHide={handleClose} size="lg" centered="true">
+                        <div className="screen">
+                            <div className="screen-header">
+                                <div className="screen-header-left">
+                                    <div className="screen-header-button close"></div>
+                                    <div className="screen-header-button maximize"></div>
+                                    <div className="screen-header-button minimize"></div>
+                                </div>
+                                <div className="screen-header-right">
+                                    <div className="screen-header-ellipsis"></div>
+                                    <div className="screen-header-ellipsis"></div>
+                                    <div className="screen-header-ellipsis"></div>
+                                </div>
+                            </div>
+                            <div className="screen-body">
+                                <div className="screen-body-item left">
+                                    <div className="app-title">
+                                        <p>CONTACT ME</p>
+                                    </div>
+                                    <div className="app-contact">
+                                        <p>Feel free to reach out to me with questions, concerns, or business requests.</p>
+                                        <p>I usually respond within 1-2 business days.</p>
+                                    </div>
+                                </div>
+                                <div className="screen-body-item">
+                                    <div className="app-form">
+                                        <div className="app-form-group">
+                                            <input className="app-form-control" placeholder="NAME" />
+                                        </div>
+                                        <div className="app-form-group">
+                                            <input className="app-form-control" placeholder="EMAIL" />
+                                        </div>
+                                        <div className="app-form-group">
+                                            <input className="app-form-control" placeholder="CONTACT NO" />
+                                        </div>
+                                        <div className="app-form-group message">
+                                            <input className="app-form-control" placeholder="MESSAGE" />
+                                        </div>
+                                        <div className="app-form-group buttons">
+                                            <button className="app-form-button" onClick={handleClose}>CANCEL</button>
+                                            <button className="app-form-button">SEND</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </Modal>
                 </Col>
             </Row>
         </Container>
     )
 }
+
+
+
